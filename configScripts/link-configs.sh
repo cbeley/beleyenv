6cadfd6ee381624b3c029b963f7faa036ef687dd
Chris Beley <chris.beley@gmail.com>
2020-07-03 21:46:48 -0700

    Add encrypted deluge hostlist + fix systemd config link bug

--- configScripts/link-configs.sh
+++ configScripts/link-configs.sh
@@ -17,12 +17,14 @@ ln -sf "$(pwd)/configs/todo.conf" ~/.todo/config
 echo "Todo.sh config installed!"
 
 # Overriden Sommelier Config
+rm -rf ~/.config/systemd
 ln -sf "$(pwd)/configs/systemd" ~/.config/systemd
 echo "systemd local overrides installed!"
 
 # Overriden Deluge Config
 mkdir -p ~/.config/deluge
 ln -sf "$(pwd)/configs/deluge/gtk3ui.conf" ~/.config/deluge/gtk3ui.conf
+ln -sf "$(pwd)/configs/deluge/hostlist.conf" ~/.config/deluge/hostlist.conf
 echo "Deluge configs installed!"
 
 echo "All configs installed!"
