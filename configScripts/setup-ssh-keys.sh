c83160d317953094ca7619d4a7db133d59d847cd
Chris Beley <chris.beley@gmail.com>
2020-07-03 17:08:37 -0700

    make setup-ssh-keys idempotent.

--- configScripts/setup-ssh-keys.sh
+++ configScripts/setup-ssh-keys.sh
@@ -3,7 +3,23 @@ set -e
 
 EMAIL=$(jq -r '.email' config.json)
 
+# Keygen returns exit one if the user says not to overwrite their
+# existing keys.  Since all scripts in this repo should be idempotent
+# and because there are legitimate reasons not to overwrite the key
+# files, we just skip SSH key config in this case.
+set +e
+
 ssh-keygen -t rsa -b 4096 -C "$EMAIL" -N '' -f ~/.ssh/id_rsa
+
+keygenExitCode=$?
+
+set -e
+
+if [[ $keygenExitCode != 0 ]]; then
+	echo "Assuming you entered 'No' when asked to ovewrite keys"
+	echo "Skipping SSH key-gen generation..."
+	exit 1
+fi
 
 # This works in both kitty & the ChromeOS terminal app.  Your milleage may
 # vary in other terminals.  It's also a bit problematic, since there seems
