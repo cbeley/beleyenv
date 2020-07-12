1b86eab21b9418a0139b601280d6f69b1bd3e37f
Christopher Beley <chris.beley@gmail.com>
2020-07-11 22:54:09 -0700

    Update install-node.sh
    was not doing apt-get update after adding new repo -- probably the root issue. github issue comment suggested removing cmdtest after adding the repo.

--- installScripts/install-node.sh
+++ installScripts/install-node.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 set -e 
 
-# See https://github.com/yarnpkg/yarn/issues/2821
-sudo apt-get remove cmdtest
-
 curl -sL https://deb.nodesource.com/setup_12.x | sudo bash -
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+
+# See https://github.com/yarnpkg/yarn/issues/2821
+sudo apt-get update
+sudo apt-get -y remove cmdtest
 
 sudo apt-get install -y nodejs yarn
 
