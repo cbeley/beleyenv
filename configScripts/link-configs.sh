fc97b2ba433510e3ee42967aaab3769a0088f526
Chris Beley <chris.beley@gmail.com>
2020-07-11 18:44:30 -0700

    * Update the README & ensure belyenv can be installed with minimal changes by anyone. * introduces 'installThingsWithEncryptedDeps' option. * Sublime license will not install if not set in config.json

--- configScripts/link-configs.sh
+++ configScripts/link-configs.sh
@@ -27,12 +27,6 @@ sudo ln -sf "$(pwd)/configs/etc/systemd/journald.conf" /etc/systemd/journald.con
 sudo systemctl restart systemd-journald
 echo "system-wide systemd configs installed!"
 
-# Deluge Configs
-mkdir -p ~/.config/deluge
-ln -sf "$(pwd)/configs/deluge/gtk3ui.conf" ~/.config/deluge/gtk3ui.conf
-ln -sf "$(pwd)/configs/deluge/hostlist.conf" ~/.config/deluge/hostlist.conf
-echo "Deluge configs installed!"
-
 # Sublime Configs
 mkdir -p ~/.config/sublime-text-3/Packages/User
 # shellcheck disable=SC2016
@@ -44,5 +38,21 @@ mkdir -p ~/.config/sublime-merge/Packages/User
 # shellcheck disable=SC2016
 find "$(pwd)/configs/sublime-merge" -maxdepth 1 -mindepth 1 -print0 | xargs -n1 -0 -I {} bash -c 'ln -sf "{}" "$HOME/.config/sublime-merge/Packages/User/$(basename "{}")"'
 echo "Sublime merge configs installed!"
+
+INSTALL_ENCRYPTED=$(jq -r '.installThingsWithEncryptedDeps' config.json)
+
+if [[ $INSTALL_ENCRYPTED = 'true' ]]; then
+    echo "Installing configs relying on encrypted files..."
+    
+    # Deluge Configs
+    mkdir -p ~/.config/deluge
+    ln -sf "$(pwd)/configs/deluge/gtk3ui.conf" ~/.config/deluge/gtk3ui.conf
+    ln -sf "$(pwd)/configs/deluge/hostlist.conf" ~/.config/deluge/hostlist.conf
+    echo "Deluge configs installed!"
+
+else
+    echo "Skipping installing configs relying on encrypted files..."
+fi
+
 
 echo "All configs installed!"
--- installScripts/installSublime/index.sh
+++ installScripts/installSublime/index.sh
@@ -6,11 +6,18 @@ echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sou
 sudo apt-get update
 sudo apt-get -y install sublime-text sublime-merge
 
-SUBLIME_LICENSE=$(jq -r '.sublimeLicense' config.json)
-mkdir -p ~/.config/sublime-text-3/Local/
-echo -e "$SUBLIME_LICENSE" > ~/.config/sublime-text-3/Local/License.sublime_license
+echo "Sublime text installed!"
 
-echo "Sublime text installed with license!"
+SUBLIME_LICENSE=$(jq -r '.sublimeLicense' config.json)
+
+if [[ $SUBLIME_LICENSE = '' ]] || [[ $SUBLIME_LICENSE = 'null' ]]; then
+    echo "No sublime license present in config.json"
+    echo "Skipping automatic license install..."
+else
+    mkdir -p ~/.config/sublime-text-3/Local/
+    echo -e "$SUBLIME_LICENSE" > ~/.config/sublime-text-3/Local/License.sublime_license
+    echo "Sublime text license installed!"
+fi
 
 mkdir -p ~/.config/sublime-text-3/Packages
 cp installScripts/installSublime/Systemd.sublime-syntax ~/.config/sublime-text-3/Packages
