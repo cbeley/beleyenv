e5c11ab83e02a407c1c5a88694a2dad68bde1aa2
Chris Beley <chris.beley@gmail.com>
2021-01-12 16:02:07 -0800

    Add docker-compose

--- installScripts/install-docker.sh
+++ installScripts/install-docker.sh
@@ -14,4 +14,10 @@ sudo usermod -aG docker "$USER"
 
 # It seems this is done by default, but adding just for safe-measure.
 sudo systemctl enable docker 
+
+sudo mkdir -p /usr/local/beleyenv/bin
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/beleyenv/bin/docker-compose
+sudo ln -sf /usr/local/beleyenv/bin/docker-compose /usr/local/bin/
+sudo chmod +x /usr/local/beleyenv/bin/docker-compose
+
 ./print.sh "Docker installed!"
