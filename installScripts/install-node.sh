131af70116d8de27ead983c75a521aa1aa0a6e19
Chris Beley <chris.beley@gmail.com>
2021-01-14 17:16:01 -0800

    Bump to latest node LTS

--- installScripts/install-node.sh
+++ installScripts/install-node.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e 
 
-curl -sL https://deb.nodesource.com/setup_12.x | sudo bash -
+curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
