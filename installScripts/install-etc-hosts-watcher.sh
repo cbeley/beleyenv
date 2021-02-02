cee6b43ad2ba2d00d7b27981df77adea8df366b5
Chris Beley <chris.beley@gmail.com>
2021-02-02 14:56:32 -0800

    Add dhcpd configuration for localhost dns

--- installScripts/install-etc-hosts-watcher.sh
+++ installScripts/install-etc-hosts-watcher.sh
@@ -17,3 +17,12 @@ systemctl --user stop etc-hosts-watcher.service
 
 systemctl --user start etc-hosts-watcher.path
 systemctl --user start etc-hosts-watcher.service
+
+# Note that we use the ^ anchor to account for the possibility the line is commented out.
+# In which case, we still want to enable it.
+if ! grep -q "^prepend domain-name-servers 127.0.0.1;" /etc/dhcp/dhclient.conf; then
+    echo 'prepend domain-name-servers 127.0.0.1;' | sudo tee -a /etc/dhcp/dhclient.conf > /dev/null
+    ./print.sh "Updated /etc/dhcp/dhclient.conf!"
+else
+    ./print.sh "/etc/dhcp/dhclient.conf already updated."
+fi
