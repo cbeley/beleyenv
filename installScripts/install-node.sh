9e755bac59e4b584154390a9f437a3e62752f4f9
Chris Beley <chris.beley@gmail.com>
2022-11-16 18:33:43 -0800

    use npm to install yarn and resolve the annoying cmdtest conflicts. Seem to be the newly recommended approach.

--- installScripts/install-node.sh
+++ installScripts/install-node.sh
@@ -5,8 +5,10 @@ curl -sL https://deb.nodesource.com/setup_19.x | sudo -E bash -
 
 # See https://github.com/yarnpkg/yarn/issues/2821
 sudo apt-get update
-sudo apt-get -y remove cmdtest
+sudo apt-get -y remove cmdtest yarn
 
-sudo apt-get install -y nodejs yarn
+sudo apt-get install -y nodejs
+
+sudo npm install -g yarn
 
 ./print.sh "Node installed!"
