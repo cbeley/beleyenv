895aa0f4dd475c2de482303bf5281a7dfc7385ff
Chris Beley <chris.beley@gmail.com>
2022-11-17 12:20:05 -0800

    Updates to ubuntu specific installs. new packages, remove bloat (are we on windows now?)

--- index.ubuntu.sh
+++ index.ubuntu.sh
@@ -4,6 +4,21 @@ set -e
 
 ./print.sh "Running Ubuntu-Specific Install Scripts!"
 
-sudo apt-get -y install steam
+sudo apt-get -y install steam guvcview \
+    gnome-shell-extension-manager gnome-tweaks \
+    ubuntu-restricted-extras openssh-server
 
 sudo snap install discord
+sudo snap install slack
+
+# snapcraft suggests it: https://snapcraft.io/discord
+snap connect discord:system-observe
+
+# Remove shit.
+# Maybe I'll use arch again and be happy...
+
+sudo apt-get -y purge thunderbird aisleriot \
+    gnome-sudoku gnome-mahjongg gnome-mines \
+    shotwell rhythmbox
+sudo apt-get -y purge 'libreoffice*'
+sudo apt-get -y auto-remove
