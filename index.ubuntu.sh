e377ecf6af2143dd269e433931a69990977f819a
Chris Beley <chris.beley@gmail.com>
2023-01-29 19:55:01 -0800

    Add flatpak support (no flatpak packages since decided not to use them, but I'll keep this here for now).

--- index.ubuntu.sh
+++ index.ubuntu.sh
@@ -7,7 +7,10 @@ set -e
 sudo apt-get -y install steam guvcview \
     gnome-shell-extension-manager gnome-tweaks \
     ubuntu-restricted-extras openssh-server \
-    ffmpegthumbnailer
+    ffmpegthumbnailer flatpak
+
+sudo flatpak remote-add --if-not-exists \
+    flathub https://flathub.org/repo/flathub.flatpakrepo
 
 sudo snap install discord
 sudo snap install slack
@@ -17,6 +20,9 @@ snap connect discord:system-observe
 
 # Remove shit.
 # Maybe I'll use arch again and be happy...
+# Note that the core install of Ubuntu does
+# not include most of this, but keeping it here
+# for completions-sake.
 
 sudo apt-get -y purge thunderbird aisleriot \
     gnome-sudoku gnome-mahjongg gnome-mines \
