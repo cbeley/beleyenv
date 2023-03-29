0ce18dff45feb8df36140645afab19eb03c18451
Chris Beley <chris.beley@gmail.com>
2023-03-29 00:01:17 -0700

    Fix install-sd cleanup

--- installScripts/install-sd.sh
+++ installScripts/install-sd.sh
@@ -3,18 +3,23 @@ set -e
 
 rm -rf temp
 mkdir temp
-cd temp
 
-curl -s https://api.github.com/repos/chmln/sd/releases/latest \
-    | jq -r '[.assets[].browser_download_url] 
-        | map(select(endswith("linux-gnu")))
-        | .[0]' \
-    | xargs wget -O sd
+(
+    cd temp
 
-chmod +x sd
+    curl -s https://api.github.com/repos/chmln/sd/releases/latest \
+        | jq -r '[.assets[].browser_download_url] 
+            | map(select(endswith("linux-gnu")))
+            | .[0]' \
+        | xargs wget -O sd
 
-sudo mkdir -p /usr/local/beleyenv/bin
-sudo cp sd /usr/local/beleyenv/bin/
-sudo ln -sf /usr/local/beleyenv/bin/sd /usr/local/bin/
+    chmod +x sd
 
-../print.sh "sd symlinked to /usr/local/bin/"
+    sudo mkdir -p /usr/local/beleyenv/bin
+    sudo cp sd /usr/local/beleyenv/bin/
+    sudo ln -sf /usr/local/beleyenv/bin/sd /usr/local/bin/
+)
+
+rm -rf temp
+
+./print.sh "sd symlinked to /usr/local/bin/"
