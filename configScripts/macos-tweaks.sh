ff98766b616f46a4a85ffd40bf3f864108bf3cd0
Christopher Beley <chris.beley@gmail.com>
2025-05-20 00:45:15 -0700

    macos tweaks: disable desktop icons

--- configScripts/macos-tweaks.sh
+++ configScripts/macos-tweaks.sh
@@ -2,4 +2,8 @@
 
 set -e
 
-defaults write com.apple.dock autohide-time-modifier -float 0.09; killall Dock
+# Speed up dock display
+defaults write com.apple.dock autohide-time-modifier -float 0.09 && killall Dock
+
+# Disable icons on the desktop
+defaults write com.apple.finder CreateDesktop -bool false && killall Finder
