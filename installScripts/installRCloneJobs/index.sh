6b8f495ff96f404a2fec916de66eed68e1c9f5ba
Christopher Beley <chris.beley@gmail.com>
2025-05-21 19:38:54 -0700

    MacOS rclone Jobs Support + local-level beleyenv config
    Introducing the idea of having a local config under .beleyenv/ that is not checked into the beleyenv repo and merges in with the config that is in the beleyenv repo. Only used for clone jobs currently.
    
    Move runRcloneJobs to new beleyenv bin and updated systemd scripts.
    
    New launchd job for MacOS.

--- installScripts/installRCloneJobs/index.sh
+++ installScripts/installRCloneJobs/index.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 set -e 
 
-sudo ln -sf "$(pwd)/installScripts/installRCloneJobs/run-rclone-jobs.sh" /usr/local/bin
-
 # See notes in install-etc-hosts-watcher.sh for why disable.
 systemctl --user disable rclone-jobs.timer
 systemctl --user enable rclone-jobs.timer
