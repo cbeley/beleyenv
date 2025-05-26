530a3178fda3d1a8b9a5f87d17abbe54f510cfab
Christopher Beley <chris.beley@gmail.com>
2025-05-26 16:01:04 -0700

    Comment out desktop disable from macos tweaks.

--- configScripts/macos-tweaks.sh
+++ configScripts/macos-tweaks.sh
@@ -5,5 +5,9 @@ set -e
 # Speed up dock display
 defaults write com.apple.dock autohide-time-modifier -float 0.09 && killall Dock
 
+# Debating whether I like this or not, so commenting out,
+# but keeping it here for future reference. This has the extra consequences
+# of being unable to do things like click the desktop to hide windows
+# and right click the desktop. 
 # Disable icons on the desktop
-defaults write com.apple.finder CreateDesktop -bool false && killall Finder
+# defaults write com.apple.finder CreateDesktop -bool true && killall Finder
